079bf554bc06d24370796664a94c85cd7f3888f4
Maya Vera <maya@mayavera.me>
2020-04-01 14:36:33 -0400

    set up basic react app

---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import App from './react/App';
+import { render } from 'react-dom';
+
+render(<App/>, document.getElementById('app'));
---
+++ webpack.config.ts
@@ -0,0 +1,45 @@
+import { resolve, join } from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config: Configuration = {
+  mode: 'development',
+  entry: './app/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".js",
+      ".jsx",
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.html?$/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
+
+export default config;
